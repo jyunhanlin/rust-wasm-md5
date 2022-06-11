206d0493ded83568ac70ea558ac587f9215acf2b
Hank Lin <jyunhanlin@gmail.com>
2022-06-11 16:38:41 +0800

    feat: add several md5 methods

--- js/index.js
+++ js/index.js
@@ -1,1 +1,83 @@
-import("../pkg/index.js").catch(console.error);
+import { md5 } from 'hash-wasm';
+import SparkMD5 from 'spark-md5';
+import nodeMd5 from 'md5';
+
+const init = async () => {
+  let rustApp;
+  let base64;
+
+  try {
+    rustApp = await import('../pkg');
+  } catch (e) {
+    console.log(e);
+  }
+  console.log({ rustApp });
+
+  const input = document.getElementById('upload');
+  const fileReader = new FileReader();
+
+  input.addEventListener('change', () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
+
+  fileReader.onloadend = () => {
+    base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+  };
+
+  const calcMd5WithMd5Wasm = () => {
+    const t0 = window.performance.now();
+    const md5Wasm = rustApp.digest(base64);
+    const t1 = window.performance.now();
+    console.log('rust-md5-wasm', { delta: t1 - t0, md5: md5Wasm });
+  };
+
+  const calcMd5WithCryptoWasm = () => {
+    const t0 = window.performance.now();
+    const md5Wasm = rustApp.md5(base64);
+    const t1 = window.performance.now();
+    console.log('rust-crypto-wasm', { delta: t1 - t0, md5: md5Wasm });
+  };
+
+  const calcMd5WithHashWasm = async () => {
+    const t0 = window.performance.now();
+    const md5Wasm = await md5(base64);
+    const t1 = window.performance.now();
+    console.log('hash-wasm', { delta: t1 - t0, md5: md5Wasm });
+  };
+
+  const calcMd5WithNodeMd5 = () => {
+    const t0 = window.performance.now();
+    const md5Web = nodeMd5(base64);
+    const t1 = window.performance.now();
+    console.log('md5', { delta: t1 - t0, md5: md5Web });
+  };
+
+  const calcMd5WithSparkMd5 = () => {
+    const t0 = window.performance.now();
+    const md5Web = SparkMD5.hash(base64);
+    const t1 = window.performance.now();
+    console.log('spark-md5', { delta: t1 - t0, md5: md5Web });
+  };
+
+  const runAll = async () => {
+    calcMd5WithMd5Wasm();
+    calcMd5WithCryptoWasm();
+    await calcMd5WithHashWasm();
+    calcMd5WithNodeMd5();
+    calcMd5WithSparkMd5();
+  };
+
+  document.getElementById('run-all').addEventListener('click', runAll);
+
+  document.getElementById('rust-md5-wasm').addEventListener('click', calcMd5WithMd5Wasm);
+
+  document.getElementById('rust-crypto-wasm').addEventListener('click', calcMd5WithCryptoWasm);
+
+  document.getElementById('hash-wasm').addEventListener('click', calcMd5WithHashWasm);
+
+  document.getElementById('md5').addEventListener('click', calcMd5WithNodeMd5);
+
+  document.getElementById('spark-md5').addEventListener('click', calcMd5WithSparkMd5);
+};
+
+document.addEventListener('DOMContentLoaded', init);
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use crypto::digest::Digest;
+use crypto::md5::Md5;
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+// use web_sys::console;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,22 +12,29 @@ use web_sys::console;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // This is like the `main` function, except for JavaScript.
-#[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
+// #[wasm_bindgen(start)]
+// pub fn main_js() -> Result<(), JsValue> {
+//     // This provides better error messages in debug mode.
+//     // It's disabled in release mode so it doesn't bloat up the file size.
+//     #[cfg(debug_assertions)]
+//     console_error_panic_hook::set_once();
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+//     // Your code goes here!
+//     console::log_1(&JsValue::from_str("Hello world!"));
 
-    Ok(())
-}
+//     Ok(())
+// }
 
 #[wasm_bindgen]
 pub fn digest(str: &str) -> String {
     let digest = md5::compute(str);
     let res = format!("{:x}", digest);
     return res;
+}
+
+#[wasm_bindgen]
+pub fn md5(input: &str) -> String {
+    let mut hasher = Md5::new();
+    hasher.input_str(input);
+    hasher.result_str()
 }
