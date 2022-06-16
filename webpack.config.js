d7694609084a1ae418b1fa819e7bba8f172001c7
Hank Lin <jyunhanlin@gmail.com>
2022-06-16 22:21:37 +0800

    feat: add css

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-const dist = path.resolve(__dirname, 'dist');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
 module.exports = {
   mode: 'production',
+
   entry: {
     index: './js/index.js',
   },
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
@@ -17,6 +19,10 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       template: './static/index.html',
+    }),
+
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: 'public' }],
     }),
 
     new WasmPackPlugin({
