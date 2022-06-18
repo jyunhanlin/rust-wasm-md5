6496fc8c5b494d1cedf316f6f93795706da3a6f4
Hank Lin <jyunhanlin@gmail.com>
2022-06-18 22:05:06 +0800

    feat: update rust to reduce wasm size

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use crypto::digest::Digest;
 use crypto::md5::Md5;
 use wasm_bindgen::prelude::*;
-// use web_sys::console;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,20 +9,6 @@ use wasm_bindgen::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-// This is like the `main` function, except for JavaScript.
-// #[wasm_bindgen(start)]
-// pub fn main_js() -> Result<(), JsValue> {
-//     // This provides better error messages in debug mode.
-//     // It's disabled in release mode so it doesn't bloat up the file size.
-//     #[cfg(debug_assertions)]
-//     console_error_panic_hook::set_once();
-
-//     // Your code goes here!
-//     console::log_1(&JsValue::from_str("Hello world!"));
-
-//     Ok(())
-// }
 
 #[wasm_bindgen]
 pub fn md5(str: &str) -> String {
